2b20ed962881c082717a105e0e5571a8a975b12c
GODINAUD Mailys <g21207635@A-F002084.salsa.univ-amu.fr>
2023-05-03 08:39:05 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.td1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.td1 to javafx.fxml;
+    exports com.example.td1;
+}
