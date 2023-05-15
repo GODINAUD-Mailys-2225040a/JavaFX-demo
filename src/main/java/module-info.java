8c08a251144acf0504b8dd4dc287d1d5b9136233
MailysGodinaudG21207635 <mailys.godinaud@etu.univ-amu.fr>
2023-05-15 11:11:47 +0200

    15/05

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,10 @@ module com.example.td1 {
     opens com.example.td1 to javafx.fxml;
     exports com.example.td1;
 
+    opens com.example.partie3 to javafx.fxml;
+
     exports com.example.partie1;
     exports com.example.partie2;
+    exports com.example.partie3;
 }
 
